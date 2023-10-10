e60e354340a445cf623f258ac62d9dc16b78cf23
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-10 18:34:01 +0100

    add JS toggle

--- atelier5/js.js
+++ atelier5/js.js
@@ -2,13 +2,22 @@ let nav = document.querySelector('.navbar');
 let toggle = document.querySelector('.fa-bars');
 
 
+$(document).ready(function () {
+  $('.fa-bars').click(function() {
+    $(this).toggleClass('fa-times')
+   
+  })
+  })
+
+
 function toggleFunction() {
   nav.classList.toggle('active');
 }
 toggle.addEventListener('click',toggleFunction )
 
-// toggle.onclick = ()=>{
-  
-//   nav.classList.add('active');
 
-// }
+
+
+
+
+
