22585d07efbd6dc30d85cf778c3a9b7c074d1f40
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-14 01:41:20 +0100

    add data in js

--- crudwithJs/js.js
+++ crudwithJs/js.js
@@ -2,5 +2,17 @@ let userName = document.getElementById('userName')
 let userEmail = document.getElementById('userEmail')
 let userNumber = document.getElementById('userNumber')
 let userMsg = document.getElementById('userMsg')
+let sendBtn = document.getElementById('sendBtn')
+let users = [];
 
 
+sendBtn.addEventListener('click',function () {
+    let user = {
+        name : userName.value,
+        email: userEmail.value,
+        number:userNumber.value,
+        message : userMsg.value
+    }
+
+    users.push(user)    
+})
