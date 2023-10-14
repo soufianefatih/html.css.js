ad9f9ca22c6c142f6b1c259107529b2f7fabb8b9
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-14 01:27:41 +0100

    add variabls in js

---
+++ crudwithJs/js.js
@@ -0,0 +1,6 @@
+let userName = document.getElementById('userName')
+let userEmail = document.getElementById('userEmail')
+let userNumber = document.getElementById('userNumber')
+let userMsg = document.getElementById('userMsg')
+
+
