537af2536d33bb73328eca64f84ddaef9a370e7d
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-16 17:52:52 +0100

    add table js

--- crudwithJs/js.js
+++ crudwithJs/js.js
@@ -23,9 +23,16 @@ sendBtn.addEventListener('click',function () {
 function showData() {
     let tableRow = '';
     for (let i = 0; i < users.length; i++) {
-        tableRow += "<tr><td>"+users[i].name+"</td><td>"+users[i].email+"</td><td>"
-        +users[i].number+"</td><td>"+users[i].message+
-        "</td><td><button class='btn btn-danger' onclick='deleteUser("+i+")'>Delete</button></td></tr>";
+        tableRow += `<tr>
+        <td>`+users[i].name+`</td>
+        <td>`+users[i].email+`</td>
+        <td>`+users[i].number+`</td>
+        <td>`+users[i].message+`</td>
+        <td>
+        <button class='btn btn-danger' onclick='deleteUser(`+i+`)'>Delete</button>
+        </td>
+        
+        </tr>`;
         
     }
     document.getElementById('tablebody').innerHTML=tableRow
