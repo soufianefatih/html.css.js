feb11c016e5a2f450f435af7fcb842c5f1276fca
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-18 19:09:01 +0100

    add func addsymbol

--- tictac/main.js
+++ tictac/main.js
@@ -14,14 +14,28 @@ let turn = true
     score:0,
  }
 
+//  win combos array
+const winCombos = [
+    [0,1,2] ,
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
 // loop to show symbol 
 for (let i = 0; i < 9; i++) {
     cells[i].addEventListener('click', ()=> {
           if (turn) {
               addSymbol(player1,i)
+              checkWin(player1)
               turn= false
           }else{
             addSymbol(player2,i)
+            checkWin(player2)
             turn = true
           }
 
@@ -33,5 +47,17 @@ for (let i = 0; i < 9; i++) {
 function addSymbol(player,i) {
 
     cells[i].innerHTML = player.symbol
+    player.played.push(i)
     
+}
+
+
+// check win o or x
+
+function checkWin(player) {
+    winCombos.some(combo => {
+        if (combo.every(items => player.played.includes(items))) {
+          alert('you can')   
+        }
+    })
 }
