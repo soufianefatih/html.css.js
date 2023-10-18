fcad4676586ee994208e0c3b3e504dc51e6c7fc8
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-18 18:52:29 +0100

    add func addsymbol

--- tictac/main.js
+++ tictac/main.js
@@ -1,14 +1,37 @@
+// all butons cells O or X
 let cells = document.querySelectorAll('.cell')
+let turn = true
+//  layers
+ let player1= {
+    symbol :'<i class="fa fa-close"></i>',
+    played: [],
+    score:0,
+ }
 
+ let player2= {
+    symbol :' <i class="fa fa-circle-o"></i>',
+    played: [],
+    score:0,
+ }
 
-
-
+// loop to show symbol 
 for (let i = 0; i < 9; i++) {
     cells[i].addEventListener('click', ()=> {
-
-       cells[i].innerHTML = '<i class="fa fa-close"></i>'
-
+          if (turn) {
+              addSymbol(player1,i)
+              turn= false
+          }else{
+            addSymbol(player2,i)
+            turn = true
+          }
 
     })
+    
+}
+
+// function to add symbol player x or o
+function addSymbol(player,i) {
+
+    cells[i].innerHTML = player.symbol
     
 }
