2f9a331ba5193e04d4b690d79796316a0ffe3237
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-26 23:05:53 +0100

    add controller index

--- gestion-user/REST-API/controller/AuthController.js
+++ gestion-user/REST-API/controller/AuthController.js
@@ -1,33 +1,28 @@
-const { User } = require("../../models");
+const { User } = require("../models");
 const { authSchema } = require("../../schemas");
-const { HttpError, BadRequestError } = require("../../helpers");
+const { HttpError, BadRequestError } = require("../helpers");
 
-const login = async (req, res, next) => {
-  const { value, error } = authSchema.loginSchema.validate(req.body, {
+const register = async (req, res, next) => {
+  const { value, error } = authSchema.registerSchema.validate(req.body, {
     abortEarly: false,
   });
+
+  const { name, email, password } = value;
+
   if (error) BadRequestError(error);
 
-  const { email, password } = value;
-  const user = await User.findOne({ email });
-  if (!user) throw HttpError(401, "Email is wrong");
-
-  if (!user.comparePassword(password)) {
-    throw HttpError(401, "Password is wrong");
+  const userEmail = await User.findOne({ email });
+  if (userEmail) {
+    throw HttpError(409, "Email has already in use");
   }
 
-  const accessToken = user.signToken();
-
-  await User.findOneAndUpdate({ email }, { accessToken });
-  res.json({
-    accessToken,
-    user: {
-      name: user.name,
-      email: user.email,
-      theme: user.theme,
-      avatarURL: user.avatarURL,
-    },
+  const result = await User.create({
+    name,
+    email,
+    password,
   });
+
+  res.status(201).json(result);
 };
 
-module.exports = login;
+module.exports = register;
