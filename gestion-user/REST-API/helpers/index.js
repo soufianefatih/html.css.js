431066dcbe5014aed5cc54081371ef9e4d833346
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-26 22:06:49 +0100

    add helpers

---
+++ gestion-user/REST-API/helpers/index.js
@@ -0,0 +1,11 @@
+const HttpError = require("./HttpError");
+const BadRequestError = require("./BadRequestError");
+const handleMongooseError = require("./handleMongooseError");
+
+
+module.exports = {
+  HttpError,
+  BadRequestError,
+  handleMongooseError,
+
+};
