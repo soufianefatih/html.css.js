0c279c2be5160826180dc69ddb8df10338c36148
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-26 21:23:28 +0100

    add server js mongo

--- gestion-user/REST-API/config/database.js
+++ gestion-user/REST-API/config/database.js
@@ -5,21 +5,19 @@ require("dotenv").config();
 const { DB_CONNECT } = process.env;
 
 const connectToMongo = async () => {
-    await mongoose.connect(DB_CONNECT);
-    console.log("Connected to MongoDB");
+    await mongoose.connect(DB_CONNECT)
+    
+.then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+  
   };
 
 
 module.exports = connectToMongo
 
 
-
-// .then(() => {
-//     app.listen(PORT);
-//     console.log("Server started at port " + PORT);
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
-  
