d0921a2c9c4cac1fd615b06738154db3694598e4
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-27 00:46:25 +0100

    add register

--- gestion-user/REST-API/controller/auth/AuthController.js
+++ gestion-user/REST-API/controller/auth/AuthController.js
@@ -3,11 +3,13 @@ const { authSchema } = require("../../schemas");
 const { HttpError, BadRequestError } = require("../../helpers");
 
 const register = async (req, res) => {
+
   const { value, error } = authSchema.registerSchema.validate(req.body, {
     abortEarly: false,
   });
+  console.log('value',{ value, error });
 
-  const { name, email, password } = value;
+  const { name, email, password } = data;
 
   if (error) BadRequestError(error);
 
@@ -25,4 +27,43 @@ const register = async (req, res) => {
   res.status(201).json(result);
 };
 
+
+
+// const register = async (req, res) => {
+//   let data = req.body;
+
+//   const user = await User.create({
+//       'name': data.name,
+//       'email': data.email,
+//       'password': data.password,
+      
+//   })
+
+//   try {
+//       res.json(user);
+//   } catch (error) {
+//       res.status(500).send(error);    
+//   }
+// }
+
+
 module.exports = register;
+
+
+// exports.register = async (req, res) => {
+//   let data = req.body;
+//   if(data.role == 'admin') res.status(400).json({message: 'this role is not available!'})
+
+//   const user = await User.create({
+//       'name': data.name,
+//       'email': data.email,
+//       'password': data.password,
+      
+//   })
+
+//   try {
+//       res.json(user);
+//   } catch (error) {
+//       res.status(500).send(error);    
+//   }
+// }
