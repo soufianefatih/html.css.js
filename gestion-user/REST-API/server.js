215b409743f85d0881e29249eade85fa54603db0
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-29 02:52:20 +0100

    add some change

--- gestion-user/REST-API/server.js
+++ gestion-user/REST-API/server.js
@@ -6,21 +6,11 @@
 require("dotenv").config();
 require('./config/database')()
 const app= require('./app');
-
-
-
 const {  PORT } = process.env;
-
-
-// console
-function ConsolLog(rep, res, next) {
-  console.log(" [" + rep["method"] + "] http://localhost:" + port + rep["url"]);
-
-  next();
-}
-
-  
 const PORTS = PORT || 3030;
+
+
+
 
 app.listen(PORTS,()=>{
 console.log(`Server is running on port ${PORT}`);
