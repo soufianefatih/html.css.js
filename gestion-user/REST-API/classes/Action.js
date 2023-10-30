92bbb15b751a275bfbc2644e26451da3cc3ed1c6
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-30 17:06:55 +0100

    add some change

--- gestion-user/REST-API/classes/Action.js
+++ gestion-user/REST-API/classes/Action.js
@@ -1,22 +1,22 @@
 class Actions {
-  static setModel(model){
-      this.model = model
-      return this;
-  }
+    static setModel(model){
+        this.model = model
+        return this;
+    }
 
-  static filters(filters) {
-      this.data = this.model.find(filters)
-      return this;
-  }
+    static filters(filters) {
+        this.data = this.model.find(filters)
+        return this;
+    }
 
-  static sorts(sorts) {
-      this.data.sort(sorts);
-      return this;
-  }
+    static sorts(sorts) {
+        this.data.sort(sorts);
+        return this;
+    }
 
-  static get(){
-      return this.data;
-  }
+    static get(){
+        return this.data;
+    }
 }
 
 module.exports = Actions
