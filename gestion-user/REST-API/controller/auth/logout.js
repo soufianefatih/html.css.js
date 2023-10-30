a5bcac3b052fefd744d55783a71278fc1c7ad69a
soufiane fatih <fatihe.soufiane@gmail.com>
2023-10-30 21:05:46 +0100

    add login

--- gestion-user/REST-API/controller/auth/logout.js
+++ gestion-user/REST-API/controller/auth/logout.js
@@ -1,8 +1,9 @@
 const { User } = require("../../models");
 
-const logout = async (req, res, next) => {
+const logout = async (req, res) => {
   const { user } = req;
   await User.findOneAndUpdate({ _id: user._id }, { accessToken: "" });
+  console.log('message :logout sucess');
   res.status(204).json();
 };
 
