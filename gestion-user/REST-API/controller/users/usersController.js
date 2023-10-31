12da4b2179e595a6238d3edda3b48668cc0cb7d4
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-01 00:31:34 +0100

    update user

--- gestion-user/REST-API/controller/users/usersController.js
+++ gestion-user/REST-API/controller/users/usersController.js
@@ -111,3 +111,69 @@ exports.up = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+exports.update = async (req, res) => {
+  try {
+    const { _id, email: oldEmail, name: oldName } = req.body;
+    console.log('usssss', req.body._id);
+    const { value, error } = userSchema.updateSchema.validate(req.body, {
+      abortEarly: false,
+    });
+
+    // Validate if _id is provided
+    const id = req.body._id;
+    if (!id) {
+      return res.status(400).json({ message: '_id is required in the request body' });
+    }
+
+    // Update user based on _id
+    const user = await User.findById(id);
+
+    // Check if user with the given _id exists
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (error) {
+      return res.status(400).json({ message: "Validation error", errors: error.details });
+    }
+
+    const { name = oldName, email, password, role = oldRole } = value;
+    const updatedUser = {
+      name,
+      role,
+    };
+
+    if (password) {
+      updatedUser.password = await hashedPassword(password);
+    }
+
+    if (email && email !== oldEmail) {
+      updatedUser.email = email;
+    }
+
+    const result = await User.findByIdAndUpdate(id, updatedUser, {
+      new: true,
+      select: "name email role",
+    });
+
+    res.json(result); // Move the response here outside of the conditions
+  } catch (err) {
+     // Log the error for debugging
+     console.error(err);
+
+    // If the error is due to an incorrect or missing _id, handle it separately
+    if (err.message.includes('_id is required')) {
+      return res.status(400).json({ message: '_id is required in the request body' });
+    }
+
+    // If the error is due to user not found, handle it separately
+    if (err.message.includes('User not found')) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
