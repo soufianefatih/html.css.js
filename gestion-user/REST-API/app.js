5b09b172383bcd0862b3b40b9c9ffe43c86fb4be
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-02 00:22:29 +0100

    logic middleware *

--- gestion-user/REST-API/app.js
+++ gestion-user/REST-API/app.js
@@ -27,7 +27,7 @@ const swaggerDocument = require("./swagger.json");
     app.use(bodyParser.json());
 
 
-
+  //* global midleware for not router
     app.use((req, res) => {
       res.status(404).json({ message: "Not Found" });
     });
