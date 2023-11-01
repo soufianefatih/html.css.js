a2f43e6f84ca9e452605a3e86de396174964bf6f
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-02 00:13:01 +0100

    logic middleware login

--- gestion-user/REST-API/controller/auth/login.js
+++ gestion-user/REST-API/controller/auth/login.js
@@ -9,24 +9,7 @@ const login =  asyncHandler (async (req, res) => {
   });
   if (error) BadRequestError(error);
 
-  const { email, password } = value;
-  const user = await User.findOne({ email });
-  if (!user) res.json(401,{message:'Email is wrong'})
-
-  if (!user.comparePassword(password)) {
-    res.json(401,{message:'Password is wrong'})
-  }
-
-  const accessToken = user.signToken();
-
-  res.json({
-    accessToken,
-    user: {
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    },
-  });
+ 
 });
 
 module.exports = login;
