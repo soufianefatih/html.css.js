5ccbe9828a1e82efd374c039dfa71cd2052d02d4
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-08 14:57:25 +0100

    add function

--- probleme solving/js1.js
+++ probleme solving/js1.js
@@ -708,3 +708,18 @@ function find(tab, val) {
 console.log(find([2, 7, 1], 7));
 console.log(find([6, 9, 5], 6));
 console.log(find([6, 7, 8], 9));
+
+// :::::::::::::::::::::::::::::Écrivez une fonction pour vérifier si un tableau contient un nombre particulier.   :::::::::::::::::::::::::::
+
+function nbrExist(tab, val) {
+	// Écrivez votre code ici
+  for(var i = 0; i < tab.length; i++){
+    if(tab[i] == val) return true;
+  }
+  return false
+}
+
+// Afficher la sortie
+console.log(nbrExist([6, 7, 8], 7));
+console.log(nbrExist([1, 2, 3], 5));
+console.log(nbrExist([], 0));
