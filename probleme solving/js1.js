6a3a9b24b8bb296ae6a02b62d1348087330e1e4a
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-08 14:55:03 +0100

    add new func

--- probleme solving/js1.js
+++ probleme solving/js1.js
@@ -678,18 +678,18 @@
 // :::::::::::::::::::::::::::::::::::: concat array   :::::::::::::::::::::::::::
 
 
-function concatener(tab1, tab2) {
+// function concatener(tab1, tab2) {
     
-	// Écrivez votre code ici
-	return  tab1.concat(tab2);
-  // return [...tab1,...tab2]
+// 	// Écrivez votre code ici
+// 	return  tab1.concat(tab2);
+//   // return [...tab1,...tab2]
 
-}
+// }
 
-// Afficher la sortie
-console.log(concatener([1, 2], [3, 4]));
-console.log(concatener([9, 8], [1, 5, 6, 7]));
-console.log(concatener([1, 1, 1], [2, 2, 2]));
+// // Afficher la sortie
+// console.log(concatener([1, 2], [3, 4]));
+// console.log(concatener([9, 8], [1, 5, 6, 7]));
+// console.log(concatener([1, 1, 1], [2, 2, 2]));
 
 
 
@@ -697,6 +697,11 @@ console.log(concatener([1, 1, 1], [2, 2, 2]));
 
 function find(tab, val) {
 	// Écrivez votre code ici
+  
+  for(var i = 0; i < tab.length; i++){
+    if(tab[i] == val) return i;
+  }
+  return -1
 }
 
 // Afficher la sortie
