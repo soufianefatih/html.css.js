2da57d4ab7abf2b7fd0097096605e7d971e6c529
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-09 23:28:45 +0100

    new state

--- my-app/src/hooks/state/State.jsx
+++ my-app/src/hooks/state/State.jsx
@@ -1,34 +1,21 @@
-// import React from 'react'
-import {useState} from 'react'
+import React from 'react'
+import { useState } from 'react'
 
-// export default function State() {
+export default function State() {
 
-//  const [value,setValue] =useState(0)
+ const [value,setValue] =useState(0)
  
-//  const updateUp = ()=> { setValue(prev => prev +1)}
-// const updateDown =() =>{setValue(prev => prev -1)}
-// const reset =()=> { setValue(0) }
+ const updateUp = ()=> { setValue(prev => prev +1)}
+const updateDown =() =>{setValue(prev => prev -1)}
+const reset =()=> { setValue(0) }
 
-//   return (
-//   <>
-//       <h1>{value}</h1>
-//       <button onClick={updateUp}> Up</button>
-//       <button onClick={reset}> reset </button> 
-//       <button onClick={updateDown}> Down</button>
-     
-//   </>
-//   )
-// }
-
-
-
-export default function state() {
- const [postes , setPostes] = useState()
-
-    const posts = fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => setPostes(data))
   return (
-    <div>state</div>
+  <>
+      <h1>{value}</h1>
+      <button onClick={updateUp}> Up</button>
+      <button onClick={reset}> reset -</button> 
+      <button onClick={updateDown}> Down</button>
+     
+  </>
   )
 }
