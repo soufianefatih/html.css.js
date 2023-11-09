1a8c740786a972372d87c4ae5fb72d1bd837cc5b
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-09 23:30:00 +0100

    new change

--- my-app/src/hooks/state/State.jsx
+++ my-app/src/hooks/state/State.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react'
 
 export default function State() {
 
- const [value,setValue] =useState(0)
+ const [value,setValue] =useState()
+ const [postes , setPostes] = useState()
+   
+ const posts = fetch('https://jsonplaceholder.typicode.com/posts')
+ .then(res => res.json())
+ .then(data => setPostes(data))
  
-
-
   return (
   <>
       <h1>data</h1>
@@ -17,14 +20,3 @@ export default function State() {
 }
 
 
-
-export default function state() {
-    const [postes , setPostes] = useState()
-   
-       const posts = fetch('https://jsonplaceholder.typicode.com/posts')
-       .then(res => res.json())
-       .then(data => setPostes(data))
-     return (
-       <div>state</div>
-     )
-   }
