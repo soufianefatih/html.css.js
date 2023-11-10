4b3d4ce946bc638fcb59f012abddff8ec6783180
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-10 20:08:05 +0100

    new data

--- my-app/src/hooks/Effect/Effect.jsx
+++ my-app/src/hooks/Effect/Effect.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Effect() {
+
+    useEffect(()=>{
+        console.log('mount');
+    },[])
   return (
     <div>Effect</div>
   )
