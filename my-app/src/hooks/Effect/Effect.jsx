dd5a594d73502f20e11f9686ef7b8aa017e675aa
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-10 20:18:36 +0100

    counter

--- my-app/src/hooks/Effect/Effect.jsx
+++ my-app/src/hooks/Effect/Effect.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 
 export default function Effect() {
 
+ const [counter , setCounter] = useState(0)
+
     useEffect(()=>{
         console.log('mount');
-    },[])
-  return (
-    <div>Effect</div>
+    },[counter])
+
+const update = () => setCounter(prev => prev + 1) 
+     
+     return (
+    <>
+       <h1>
+        {counter}
+       </h1>
+       <button onClick={update}>Click</button>
+    </>
   )
 }
 
