6d3d8071e95daff53f3174fcc6fc069949c8d643
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-10 20:03:46 +0100

    add cmponent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import State from './hooks/state/State';
+import Effect from './hooks/Effect/Effect';
+// import State from './hooks/state/State';
 
 function App() {
   return (
      <>
-     <State/>
+     {/* <State/> */}
+     <Effect/>
+
      </>     
     )
   
