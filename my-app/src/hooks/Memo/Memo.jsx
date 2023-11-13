0d170a296e2f4b276c10597d63b13795341c811a
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-13 22:39:43 +0100

    changes

--- my-app/src/hooks/Memo/Memo.jsx
+++ my-app/src/hooks/Memo/Memo.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react"
 
 export default function Memo() {
+    const [counterOne , setCounterOne] = useState(0)
+    const [counterTwo , setCounterTwo] = useState(0)
+    const changeOne = ()=> {setCounterOne(prev=>prev+1)}
+    const changeTwo = ()=> {setCounterTwo(prev=>prev-1)}
+   
+    const isEven =  ()=> {
+        return counterOne % 2 === 0
+    }
+
   return (
-    <div>Memo</div>
+    <div>
+    <button onClick={changeOne}>{counterOne }</button> 
+    {isEven() ? 'Even' : 'Odd'}
+    <button onClick={changeTwo}>{counterTwo}</button> 
+
+    </div>
   )
 }
