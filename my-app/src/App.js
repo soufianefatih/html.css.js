5d5d5cfde4c634515fea2721d908dedb106a9971
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-13 22:11:43 +0100

    add file hook ref

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Effect from './hooks/Effect/Effect';
+// import Effect from './hooks/Effect/Effect';
+import Ref from './hooks/Ref/Ref';
 // import State from './hooks/state/State';
 
 function App() {
   return (
      <>
      {/* <State/> */}
-     <Effect/>
+     <Ref/>
 
      </>     
     )
--- my-app/src/hooks/Ref/Ref.js
+++ my-app/src/hooks/Ref/Ref.js
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function Ref() {
   return (
-    <div>Ref</div>
+    <>
+    
+    <h1>GO TO PARAGHRAPH</h1>
+    <div>
+        <section>
+            <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
+            <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
+        </section>
+        <section>
+            <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
+            <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
+        </section> 
+         <section>
+            <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
+            <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
+        </section>
+        <section>
+            <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
+            <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
+        </section>
+    </div>
+    
+    
+    
+    </>
+   
   )
 }
