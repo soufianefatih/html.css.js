4d98d13eea396612ce9fef546403c6f527b34944
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-13 22:15:30 +0100

    add some change

--- my-app/src/hooks/Ref/Ref.js
+++ my-app/src/hooks/Ref/Ref.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 export default function Ref() {
+
+const moveDown = ()=> {
+    console.log('down');
+}
+
+
   return (
     <>
     
     <h1>GO TO PARAGHRAPH</h1>
+    <button onClick={moveDown}>move down</button>
     <div>
         <section>
             <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
@@ -15,6 +22,14 @@ export default function Ref() {
             <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
         </section> 
          <section>
+            <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
+            <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
+        </section>
+        <section>
+            <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
+            <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
+        </section>
+        <section>
             <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
             <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
         </section>
