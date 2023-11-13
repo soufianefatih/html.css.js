f6c0585c4ebed1ea29fe57da09dc22e59ce69717
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-13 22:44:26 +0100

    add data

--- my-app/src/hooks/Memo/Memo.jsx
+++ my-app/src/hooks/Memo/Memo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 export default function Memo() {
     const [counterOne , setCounterOne] = useState(0)
@@ -6,14 +6,20 @@ export default function Memo() {
     const changeOne = ()=> {setCounterOne(prev=>prev+1)}
     const changeTwo = ()=> {setCounterTwo(prev=>prev-1)}
    
-    const isEven =  ()=> {
+  const isEven =  useMemo(()=>{
+     let i =0
+     while (i< 2000000000000) i++
         return counterOne % 2 === 0
-    }
+    },[counterOne])
+   
+    // const isEven =  ()=> {
+    //     return counterOne % 2 === 0
+    // }
 
   return (
     <div>
     <button onClick={changeOne}>{counterOne }</button> 
-    {isEven() ? 'Even' : 'Odd'}
+    {isEven ? 'Even' : 'Odd'}
     <button onClick={changeTwo}>{counterTwo}</button> 
 
     </div>
