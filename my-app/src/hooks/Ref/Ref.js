aac17aaa60f1b25883b1fc9fbb1a5b75888d4fa0
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-13 22:19:23 +0100

    add buton

--- my-app/src/hooks/Ref/Ref.js
+++ my-app/src/hooks/Ref/Ref.js
@@ -1,9 +1,9 @@
 import { useRef } from "react";
 export default function Ref() {
-    
+    const lastPara = useRef() 
 
 const moveDown = ()=> {
-    console.log('down');
+    console.log(lastPara.current);
 }
 
 
@@ -35,7 +35,7 @@ const moveDown = ()=> {
         </section>
         <section>
             <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png"/>
-            <p>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
+            <p  ref= {lastPara}>lorem ipsum dolor fggjjf fkgkgkg rgkfk fkkffk fkffkf ffffkk dfffff  ffffff vfkkfkg</p>
         </section>
     </div>
     
