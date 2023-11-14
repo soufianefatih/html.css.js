b242e30cdaf7dad062501781ed55074b8ca523ec
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-14 22:46:14 +0100

    add new change .env

--- upload-file/final/app.js
+++ upload-file/final/app.js
@@ -40,7 +40,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
