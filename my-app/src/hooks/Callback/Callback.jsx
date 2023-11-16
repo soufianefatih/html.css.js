a17f89209ca76bb2209c8a9230e193041c8daa5d
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-16 21:49:50 +0100

    add button

--- my-app/src/hooks/Callback/Callback.jsx
+++ my-app/src/hooks/Callback/Callback.jsx
@@ -2,17 +2,29 @@ import React, { useState } from 'react'
 import Age from './Age'
 import Salary from './Salary'
 import"./callback.css"
+import Button from './Button'
 
 export default function Callback() {
     const [age , setAge] = useState(20)
     const [salary, setSalary] = useState(1000)
     
   return (
-    <div className='red'>
+   <>
+   
+   <div className='red'>
      <Age age ={age}/>
      <Salary salary ={salary}/>
 
     </div>
+    <div className='blue'>
+    <Button text = {'Age Up'}/>
+    <Button text = {'Salary Up'}/>
+
+    </div>
+   
+   
+   
+   </>
   )
 
 }
