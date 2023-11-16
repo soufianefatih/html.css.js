a917dbab5270bfbdb3dd3e229e23282302ec7e59
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-16 21:31:14 +0100

    ADD STATE

--- my-app/src/hooks/Callback/Callback.jsx
+++ my-app/src/hooks/Callback/Callback.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Callback() {
+    const [age , setAge] = useState(20)
+    const [salary, setSalary] = useState(1000)
+    
   return (
     <div>Callback</div>
   )
