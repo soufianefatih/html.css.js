9370eb6ff51deb97d282c4c7f58fc191ac716f0f
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-19 20:45:54 +0100

    add

--- my-app/src/hooks/Context/CurentUser.jsx
+++ my-app/src/hooks/Context/CurentUser.jsx
@@ -1,10 +1,11 @@
 import React, {useContext} from 'react'
+import { UserContext } from './Context'
 
 export default function CurentUser() {
 
+const {userOne} = useContext(UserContext)
 
 
-    
   return (
     <div></div>
   )
