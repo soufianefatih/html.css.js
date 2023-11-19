7c85956b8dfa58bf83b89e55b81275310ec85c39
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-19 20:59:11 +0100

    add

--- my-app/src/hooks/Context/Button.jsx
+++ my-app/src/hooks/Context/Button.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import  UserContext  from './UserContext'
+
 
 export default function Button() {
+    const {setUserOne} = useContext(UserContext)
+
   return (
     <button onClick={()=>{
         setUserOne("soufiane")
