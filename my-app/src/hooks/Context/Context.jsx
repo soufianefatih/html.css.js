5afc4c37aefd7fab43100377f808aea848736468
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-19 20:26:56 +0100

    state

--- my-app/src/hooks/Context/Context.jsx
+++ my-app/src/hooks/Context/Context.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Context() {
+    const [yserOne , setUserOne] = useState('majed')
+
   return (
     <div>Context</div>
   )
