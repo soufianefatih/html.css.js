1fdea4e6a558dda5b284a959c83245639363e714
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-19 20:55:24 +0100

    add button

--- my-app/src/hooks/Context/User.jsx
+++ my-app/src/hooks/Context/User.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from './Card'
 import CurentUser from './CurentUser'
+import Button from './Button'
 
 export default function User() {
   return (
     <Card>
     <CurentUser /> 
+    <Button/>
    </Card>
   )
 }
