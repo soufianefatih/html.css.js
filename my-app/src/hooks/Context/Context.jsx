5850eb299e2028b5b4c2cb5d0beb23ee750cd150
soufiane fatih <fatihe.soufiane@gmail.com>
2023-11-19 20:39:25 +0100

    add

--- my-app/src/hooks/Context/Context.jsx
+++ my-app/src/hooks/Context/Context.jsx
@@ -1,6 +1,9 @@
 import React, { useState ,createContext} from 'react'
 import User from './User'
 
+
+const UserContext = 
+
 export default function Context() {
     const [userOne , setUserOne] = useState('majed')
 
